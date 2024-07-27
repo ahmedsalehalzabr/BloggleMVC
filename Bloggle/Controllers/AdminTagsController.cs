3261dd2dcd22cf4c80671c31df7faca7b5021116
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-07-27 01:10:01 -1200

    Tag Add

---
+++ Bloggle/Controllers/AdminTagsController.cs
@@ -0,0 +1,36 @@
+﻿using Bloggle.Data;
+using Bloggle.Models.Domain;
+using Bloggle.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggle.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly AppDbContext appDbContext;
+
+        public AdminTagsController(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Add")]
+        public async Task<IActionResult> Add(AddTagRequest addTagRequest)
+        {
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+         await appDbContext.Tags.AddAsync(tag);
+         await appDbContext.SaveChangesAsync();
+            return View("Add");
+        }
+    }
+}
